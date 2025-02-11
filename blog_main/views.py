7963921b95b96541fbd4048bb120382a1f707c1d
Rohan <rohanflash27@gmail.com>
2025-02-11 23:03:37 +0530

    added new fethure

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,15 @@
 
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 
 def home(request):
-    return render(request,'home.html')
+    featured_post=Blog.objects.filter(is_fetured=True,status='Published').order_by('-updated_at')
+    recent_post=Blog.objects.filter(is_fetured=False,status='Published').order_by('-updated_at')
+    print(recent_post)
+    context={
+        'featured_post':featured_post,
+        'recent_post':recent_post,
+    }
+    return render(request,'home.html',context)
--- blogs/admin.py
+++ blogs/admin.py
@@ -3,6 +3,8 @@ from . models import Category,Blog
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug': ('title',)}
-
+    list_display=('title','category','author','status','is_fetured')
+    search_fields=('id','title','category__Category_name','status')
+    list_editable=('is_fetured','status')
 admin.site.register(Category)
 admin.site.register(Blog,BlogAdmin)
