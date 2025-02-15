b271b584d018df5e6deacf30c7aff7ab644507b0
Rohan <rohanflash27@gmail.com>
2025-02-15 18:35:02 +0530

    auth

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,10 @@
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from blog_main.forms import Registrationform
 from blogs.models import About, Blog, Links
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def home(request):
@@ -18,3 +21,38 @@ def home(request):
         'about':about,
     }
     return render(request,'home.html',context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form=Registrationform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:
+        form=Registrationform()
+    context={
+        'form':form,
+    }
+    return render(request,'register.html',context)
+
+def login(request):
+    if request.method=='POST':
+        form=AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username= form.cleaned_data['username']
+            password= form.cleaned_data['password']
+
+            user=auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+            return  redirect('home') 
+    form=AuthenticationForm()
+    context={
+        'form':form
+    }
+    return render(request,'login.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
