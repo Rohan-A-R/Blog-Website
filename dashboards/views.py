4a8de5b8c27c03cf8314fa7cf8bea213a28e0d3c
Rohan <rohanflash27@gmail.com>
2025-02-19 06:44:28 +0530

    edit/delete

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm
@@ -15,11 +15,11 @@ def dashboard(request):
     }
     return render(request,'dashboard/dashboard.html',context)
 
-
+@login_required(login_url='login')
 def categories(request):
     return render(request,'dashboard/categories.html')
 
-
+@login_required(login_url='login')
 def add_category(request):
     if request.method=='POST':
         form=CategoryForm(request.POST)
@@ -32,5 +32,24 @@ def add_category(request):
     }
     return render(request,'dashboard/addcategory.html',context)
 
+@login_required(login_url='login')
 def edit_category(request,pk):
-    return render(request,'dashboard/edit_category.html')
+    category=get_object_or_404(Category,pk=pk)
+    if request.method=='POST':
+        form=CategoryForm(request.POST,instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form=CategoryForm(instance=category)
+    context={
+        'form':form,
+        'category':category
+    }
+    return render(request,'dashboard/edit_category.html',context)
+
+
+def delete_category(request,pk):
+    category=get_object_or_404(Category,pk=pk)
+    category.delete()
+    return redirect('categories')
+
